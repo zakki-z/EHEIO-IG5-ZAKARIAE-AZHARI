8842d95fb0569ce35a25ffbc7bafd7ec13bf70a0
zaki azhari <zakariaeazhari@proton.me>
2026-01-18 00:11:02 +0100

    complete work

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -17,16 +17,58 @@ class DummyController extends AbstractController
             'controller_name' => 'DummyController',
         ]);
     }
-    #[Route(path: '/dummy/home', name: 'app_home_dummy')]
+
+    #[Route('/home', name: 'app_home')]
     public function home(): Response
     {
-    $text = "Hello This is home !";
-    $tabYear = [2020,2021,2022,2023];
-    return $this->render('dummy/home.html.twig', [
-        "text" => $text,
-        "years" => $tabYear
-    ]);
-}
+        $text = "Hello This is home !";
+        $tabYear = [2020, 2021, 2022, 2023];
+        return $this->render('dummy/home.html.twig', [
+            "text" => $text,
+            "years" => $tabYear
+        ]);
+    }
+
+    #[Route('/news', name: 'app_news')]
+    public function news(): Response
+    {
+        $newsTitle = "Latest News";
+        $articles = [
+            "Symfony 7.0 Released",
+            "PHP 8.3 New Features",
+            "Twig Template Updates",
+            "Doctrine ORM Best Practices"
+        ];
+        return $this->render('dummy/news.html.twig', [
+            "title" => $newsTitle,
+            "articles" => $articles
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        $contactInfo = [
+            'email' => 'contact@example.com',
+            'phone' => '+212 123 456 789',
+            'address' => 'Oujda, Morocco'
+        ];
+        return $this->render('dummy/contact.html.twig', [
+            "contactInfo" => $contactInfo
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        $aboutText = "Welcome to our Symfony application. We are learning Twig templates!";
+        $team = ['Mohammed', 'Ahmed', 'Fatima', 'Youssef'];
+        return $this->render('dummy/about.html.twig', [
+            "aboutText" => $aboutText,
+            "team" => $team
+        ]);
+    }
+
     #[Route('/add-book', name: 'app_book_add')]
     public function book(): Response
     {
@@ -36,5 +78,4 @@ class DummyController extends AbstractController
             'bookForm' => $bookForm
         ]);
     }
-
 }
